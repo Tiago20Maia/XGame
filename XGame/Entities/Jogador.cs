33688f298691fd811ff8a205c63300e8a14b08dc
tiagolenovo <tiagoferreira209@gmail.com>
2020-03-10 20:21:14 -0300

    Inicio do projeto, Entities, Enum e ValueObject

---
+++ XGame/Entities/Jogador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XGame.Enum;
+using XGame.ValueObject;
+
+namespace XGame.Entities
+{
+    public class Jogador
+    {
+        public Guid Id { get; set; }
+        public Nome Nome { get; set; }
+        public Email Email { get; set; }
+        public string Senha { get; set; }
+        public EnumSituacaoJogador status { get; set; }
+
+    }
+}
---
+++ XGame/Entities/JogoPlataforma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XGame.Entities
+{
+    public class JogoPlataforma
+    {
+        public Guid Id { get; set; }
+        public Plataforma Plataforma { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public Jogo Jogo { get; set; }
+    }
+}
