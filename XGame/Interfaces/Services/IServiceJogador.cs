5f16ac0f5237160663485e48f376e58013eb51f1
tiagolenovo <tiagoferreira209@gmail.com>
2020-03-10 21:49:10 -0300

    Adicionado Interfaces e Services

---
+++ XGame/Interfaces/Services/IServiceJogador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XGame.Arguments.Jogador;
+
+namespace XGame.Interfaces.Services
+{
+    public interface IServiceJogador
+    {
+        AutenticarJogadorResponse AutenticarJogador(AutenticarJogadorRequest request);
+        AdicionarJogadorResponse AdicionarJogador(AdicionarJogadorRequest request);
+    }
+}
---
+++ XGame/Interfaces/Services/IServicePlataforma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XGame.Arguments.Jogador;
+
+namespace XGame.Interfaces.Services
+{
+    public interface IServicePlataforma
+    {
+        AdicionarPlataformaResponse AdicionarJogador(AdicionarPlataformaRequest request);
+    }
+}
